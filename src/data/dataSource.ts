f7617840f1ccd550e9ffb59066b7f0d60769a475
Craig <indigomx9@gmail.com>
2022-05-30 13:53:26 -0400

    Gettting ready for Heroku deployment.

---
+++ src/data/dataSource.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+
+export const dataSource = new DataSource({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: true,
+    logging: false,
+    entities: ["dist/models/**/*.js"],
+    migrations: ["dist/migrations/**/*.js"],
+    subscribers: ["dist/subscribers/**/*.js"]
+});
+
+
+
